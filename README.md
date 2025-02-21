e32aca0b9cad69ea4ec1b34b42c248861f142b05
noveon-cguillermo <cguillermo@noveon.co>
2025-02-21 15:01:35 -0600

    Lesson3: animated the home screeen with various motion and animate elements

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+THIS PROJECT USES NODE 16
 ## Available Scripts
 
 In the project directory, you can run:
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion }from 'framer-motion'
+
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
-      <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
-      </Link>
-    </div>
+    // <motion.div className="home container"
+    //   animate={{marginTop: 200, opacity: 0.2, rotateZ: 180}}>
+    //   <motion.h2 animate={{ fontSize: 50, color: '#f22494', x: 100, y: -100 }}>
+    //     Welcome to Pizza Joint
+    //   </motion.h2>
+    //   <Link to="/base">
+    //     <motion.button
+    //     animate={{ scale: 1.5 }}>
+    //       Create Your Pizza
+    //     </motion.button>
+    //   </Link>
+    // </motion.div>
+    <motion.div className="home container"
+    animate={{marginTop: 200, opacity: 0.5, rotateZ: 360}}>
+    <motion.h2 animate={{ fontSize: 50, color: '#f22494', x: 100, y: -100 }}>
+      Welcome to Pizza Joint
+    </motion.h2>
+    <Link to="/base">
+      <motion.button
+      animate={{ scale: 1.5 }}>
+        Create Your Pizza
+      </motion.button>
+    </Link>
+  </motion.div>
   )
 }
 
