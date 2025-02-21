75d786db7f4e5d84677357583dc0e85012e19fe5
noveon-cguillermo <cguillermo@noveon.co>
2025-02-21 16:17:13 -0600

    Lesson 8: Children variant properties

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import { motion }from 'framer-motion'
+
+const containerVariants = { // Defining motion properties before hand
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      mass: 0.4, // higher is faster 
+      damping: 8, // higher lesss oscillation
+      when: 'beforeChildren', //complete this animation before any children animation occur
+      staggerChildren: 0.4 //animation time between children
+    }
+  }
+}
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1
+  }
+}
 
 const Order = ({ pizza }) => {
   return (
-    <div className="container order">
+    <motion.div className="container order"
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
+      <motion.p
+        variants={childVariants}
+      >You ordered a {pizza.base} pizza with:</motion.p>
+      <motion.div
+        variants={childVariants}
+      >
+        {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+      </motion.div>
+    </motion.div>
   )
 }
 
