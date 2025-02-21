69cb4d6fe31ff3b2c3777093fcd6f1dd0362253e
noveon-cguillermo <cguillermo@noveon.co>
2025-02-21 15:30:25 -0600

    Lesson 6: Added hover animations

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { motion }from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
@@ -16,9 +16,12 @@ const Base = ({ addBase, pizza }) => {
         {bases.map(base => {
           let spanClass = pizza.base === base ? 'active' : '';
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li key={base} onClick={() => addBase(base)}
+              whileHover={{ scale: 1.1, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
+            >
               <span className={spanClass}>{ base }</span>
-            </li>
+            </motion.li>
           )
         })}
       </ul>
@@ -29,7 +32,12 @@ const Base = ({ addBase, pizza }) => {
           animate= {{ x: 0}}
           transition={{ type: 'spring', stiffness: 120}}>
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button
+              whileHover={{ 
+                scale: 1.1,
+                textShadow: '0px 0px 8px rgb(255,255,255)',
+                boxShadow: '0px 0px 8px rgb(255,255,255)'
+              }}>Next</motion.button>
           </Link>
         </motion.div>
       )}
