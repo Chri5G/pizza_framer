ccacafd8489879d9e6b328b9137021f02e6c0504
noveon-cguillermo <cguillermo@noveon.co>
2025-02-26 14:36:09 -0600

    impelemented UseCycle Hook to change the loader animation

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
   animationOne: {
@@ -16,18 +16,35 @@ const loaderVariants = {
         ease: 'easeOut'
       }
     }
+  },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      y: {
+        yoyo: Infinity,
+        duation: 0.25,
+        ease: 'easeOut'
+      }
+    }
   }
 }
 
 const Loader = () => {
+
+  const [animation, cycleAnimation] = useCycle('animationOne', 'animationTwo'); // cycle b/t the 2 variants, default is 1st one
+
+
   return (
     <>
     <motion.div className="loader"
       variants={loaderVariants}
-      animate='animationOne'
+      animate={animation}
     >
-
     </motion.div>
+    <div onClick={() => cycleAnimation()}>{/*  No arguments needed*/}
+      Cycle Loader
+    </div>
     </>
   )
 }
