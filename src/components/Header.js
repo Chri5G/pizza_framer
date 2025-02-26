16ed27ef5803131c46334608555fdc20e089408e
noveon-cguillermo <cguillermo@noveon.co>
2025-02-26 14:12:54 -0600

    animated the logo svg with pathlength, svg gets drawn

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,21 +2,52 @@ import React from 'react';
 import { motion }from 'framer-motion'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
+const svgVariants = {
+  hidden: {
+    rotate: -180
+  },
+  visible: {
+    rotate: 0,
+    transition: {duration: 1}
+  }
+}
+
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0, // length of the path, pen will be hovering over intial position
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1, // draws the svg
+    transition: { 
+      duration: 2,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
     <header>
       <Link to="/">
         <div className="logo">
-          <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-            <path
+          <motion.svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"
+            variants={svgVariants}
+            initial='hidden'
+            animate='visible'
+          >
+            <motion.path
+              variants={pathVariants}
               fill="none"
-              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+              d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z" /* Pac Man*/
             />
-            <path
+            <motion.path
+              variants={pathVariants}
               fill="none"
-              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+              d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" /* Top right part */
             />
-          </svg>
+          </motion.svg>
         </div>
       </Link>
       <motion.div className="title"
